c1b5c30a9afc716ec43bf0cf35ecac1b80096bc6
dsfiles <63224731+dsfiles@users.noreply.github.com>
2020-10-15 13:19:09 -0500

    Update fork1.c

--- week07/fork1.c
+++ week07/fork1.c
@@ -8,13 +8,13 @@ int main() {
   pid = fork(); // clone a process
   if (pid < 0) { /* error occurred (very unlikely in your VM) */
     fprintf(stderr, "Fork Failed"); //send error message to stderr (screen or a file)
-  return 1;
+    return 1;
   } else if (pid == 0) { /* child process */
     printf("child process successfully created!\n");
-    printf ("Child process: PID = %d, parent PID = %d\n", getpid(), getppid( ) );
+    printf ("Child process: PID = %d, parent PID = %d\n", getpid(), getppid());
   }
   else { /* parent process */
-    printf ("Parent: PID = %d, parent_PID = %d\n", getpid(), getppid( ) );
+    printf ("Parent process: PID = %d, parent_PID = %d\n", getpid(), getppid());
     wait(NULL);
   }
   return 0;
