2da97d662269d63e20980bb88f4e4eb2aff64e59
dsfiles <63224731+dsfiles@users.noreply.github.com>
2020-10-15 12:39:52 -0700

    Update fork2.c

--- week07/fork2.c
+++ week07/fork2.c
@@ -13,11 +13,10 @@ int main()
 		return 1;
 	} else if (pid == 0) { /* child process */
 		execlp("/bin/ls","ls",NULL);
-	} else { /* parent process */
-	
-	/* parent will wait for the child to complete */
-	wait(NULL);
-	printf("Child Complete");
+	} else { /* parent process */	
+		* parent will wait for the child to complete */
+		wait(NULL);
+		printf("Child Complete");
 	}
 	return 0;
 }
