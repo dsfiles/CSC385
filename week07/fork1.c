c2b158fdd27ff611f4812e071a62ee548807ca19
dsfiles <63224731+dsfiles@users.noreply.github.com>
2020-10-15 12:41:05 -0500

    Create fork1.c

---
+++ week07/fork1.c
@@ -0,0 +1,22 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+  pid t pid, pid1;
+  /* fork a child process */
+  pid = fork(); // clone a process
+  if (pid < 0) { /* error occurred (very unlikely in your VM) */
+    fprintf(stderr, "Fork Failed"); //send error message to stderr (screen or a file)
+  return 1;
+  } else if (pid == 0) { /* child process */
+    printf("child process successfully created!\n");
+    printf ("child_PID = %d,parent_PID = %d\n", getpid(), getppid( ) );
+  }
+  else { /* parent process */
+    printf("Parent process:\n");
+    printf ("child_PID = %d,parent_PID = %d\n", getpid(), getppid( ) );
+    wait(NULL);
+  }
+  return 0;
+}
