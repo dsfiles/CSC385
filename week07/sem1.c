91b98315af2690c5da790f7b7cfe2b1da467af5a
dsfiles <63224731+dsfiles@users.noreply.github.com>
2020-10-16 20:45:23 -0500

    Update sem1.c

--- week07/sem1.c
+++ week07/sem1.c
@@ -9,10 +9,10 @@ void* thread_function(void* arg)
 { 
     //wait 
     sem_wait(&sem); 
-    printf("\nEnter critical section..\n"); 
+    printf("\nEntering critical section..\n"); 
   
     //critical section 
-    printf("\Now inside critical section..\n"); 
+    printf("\nNow inside critical section..\n"); 
   //sleep(1); 
       
     //signal 
@@ -27,7 +27,7 @@ int main()
     pthread_t thread1,thread2; 
     pthread_create(&thread1,NULL,thread_function,NULL); 
     sleep(1); 
-    pthread_create(&t2,NULL,thread_function,NULL); 
+    pthread_create(&thread2,NULL,thread_function,NULL); 
     pthread_join(thread1,NULL); 
     pthread_join(thread2,NULL); 
     sem_destroy(&sem); 
