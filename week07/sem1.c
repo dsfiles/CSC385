ef07e77ceaa80bb1285209e29276e46522b6ae32
dsfiles <63224731+dsfiles@users.noreply.github.com>
2020-10-16 20:55:03 -0500

    Update sem1.c

--- week07/sem1.c
+++ week07/sem1.c
@@ -3,33 +3,33 @@
 #include <semaphore.h> 
 #include <unistd.h> 
   
-sem_t sem; //define a semaphore
+sem_t s; //define a semaphore used as a mutex
   
 void* thread_function(void* arg) 
 { 
     //wait 
-    sem_wait(&sem); 
+    sem_wait(&s); 
     printf("\nEntering critical section..\n"); 
   
     //critical section 
-    printf("\nNow inside critical section..\n"); 
+    printf("Now inside critical section..\n"); 
   //sleep(1); 
       
     //signal 
     printf("\nExiting...\n"); 
-    sem_post(&sem); 
+    sem_post(&s); 
 } 
   
   
 int main() 
 { 
-    sem_init(&sem, 0, 1); // initiazlize sem with a value 1
+    sem_init(&s, 0, 1); // initiazlize sem with a value 1
     pthread_t thread1,thread2; 
     pthread_create(&thread1,NULL,thread_function,NULL); 
     sleep(1); 
     pthread_create(&thread2,NULL,thread_function,NULL); 
     pthread_join(thread1,NULL); 
     pthread_join(thread2,NULL); 
-    sem_destroy(&sem); 
+    sem_destroy(&); 
     return 0; 
 } 
