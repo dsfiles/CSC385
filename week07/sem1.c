ec88f1d7e9b054f96065c3665fcbb3521244ea89
dsfiles <63224731+dsfiles@users.noreply.github.com>
2020-10-16 20:42:19 -0500

    Update sem1.c

--- week07/sem1.c
+++ week07/sem1.c
@@ -5,7 +5,7 @@
   
 sem_t sem; //define a semaphore
   
-void* thread(void* arg) 
+void* thread_function(void* arg) 
 { 
     //wait 
     sem_wait(&sem); 
@@ -24,9 +24,9 @@ int main()
 { 
     sem_init(&sem, 0, 1); // initiazlize sem with a value 1
     pthread_t thread1,thread2; 
-    pthread_create(&thread1,NULL,thread,NULL); 
+    pthread_create(&thread1,NULL,thread_function,NULL); 
     sleep(1); 
-    pthread_create(&t2,NULL,thread,NULL); 
+    pthread_create(&t2,NULL,thread_function,NULL); 
     pthread_join(thread1,NULL); 
     pthread_join(thread2,NULL); 
     sem_destroy(&sem); 
