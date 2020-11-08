e6459a805ef8644d08eab540a283a212868254b5
dsfiles <63224731+dsfiles@users.noreply.github.com>
2020-11-07 21:35:08 -0800

    Update producer-consumer.c

--- week10/producer-consumer.c
+++ week10/producer-consumer.c
@@ -7,24 +7,24 @@ This is a skeleton for the producer-consumer problem using mutex and semaphore.
 #include <stdlib.h>
 #include <stdio.h>
 
-const int max = 5; //maximum items
-const int buffer_size = 5;
+#define MAX 5; //maximum items
+#define BUFFSIZE 5;
 
 sem_t empty;
 sem_t full;
-int buffer[buffer_size];
+int buffer[BUFFSIZE];
 pthread_mutex_t mutex;
 int in = 0;
 int out = 0;
 
 void *producer(void *p)
 {   
-    for(int i = 0; i < max; i++) {
+    for(int i = 0; i < MAX; i++) {
         sem_wait(&empty);
         pthread_mutex_lock(&mutex);
         buffer[in] = i;
         printf("producer %d: insert item %d at\tbuffer[%d]\n", *((int *)p),buffer[in],in);
-        in = (in+1)%buffer_size;
+        in = (in+1)%BUFFSIZE;
         pthread_mutex_unlock(&mutex);
         sem_post(&full);
     }
@@ -32,12 +32,12 @@ void *producer(void *p)
 
 void *consumer(void *c)
 {   
-    for(int i = 0; i < max; i++) {
+    for(int i = 0; i < MAX; i++) {
         sem_wait(&full);
         pthread_mutex_lock(&mutex);
         int item = buffer[out];
         printf("consumer %d: remove item %d from buffer[%d]\n",*((int *)c),item, out);
-        out = (out+1)%buffer_size;
+        out = (out+1)%BUFFSIZE;
         pthread_mutex_unlock(&mutex);
         sem_post(&empty);
     }
@@ -48,7 +48,7 @@ int main()
 
     pthread_t producer[5],consumer[5];
     pthread_mutex_init(&mutex, NULL);
-    sem_init(&empty,0,buffer_size);
+    sem_init(&empty, 0, BUFFSIZE);
     sem_init(&full,0,0);
 
     int a[5] = {1,2,3,4,5}; //Just used for numbering the producer and consumer
